78cc6adee5b1b6dc3d962d1f5660bbffa1a18a8b
Allu336 <alisa.t1@outlook.com>
2021-08-21 21:01:08 +0300

    env

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask import render_template, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///ally"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
